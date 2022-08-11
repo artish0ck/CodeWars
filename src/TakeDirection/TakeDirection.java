c1888f457b3b082b4548f9ab0840708c33d0248b
Artjoms Puzina <artjomspuzina@gmail.com>
2022-08-11 14:29:33 +0300

    tasks on 11/8/22 add

---
+++ src/TakeDirection/TakeDirection.java
@@ -0,0 +1,39 @@
+package TakeDirection;
+
+/*
+You live in the city of Cartesia where all roads are laid out in a perfect grid.
+You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk.
+The city provides its citizens with a Walk Generating App on their phones -- everytime
+you press the button it sends you an array of one-letter strings representing directions
+to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it
+takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives
+you will take you exactly ten minutes (you don't want to be early or late!) and will,
+of course, return you to your starting point. Return false otherwise.
+
+Note: you will always receive a valid array containing a random assortment of direction
+letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+ */
+
+public class TakeDirection {
+    public static void main(String[] args) {
+        char[] directions = {'n', 's', 'n', 's', 'e', 'w', 'e', 'e', 'w'};
+        System.out.println(isValid(directions));
+    }
+
+    public static boolean isValid(char[] walk) {
+        if (walk.length != 10) {
+            return false;
+        }
+        int verticalDirection = 0;
+        int horizontalDirection = 0;
+        for (char char1 : walk) {
+            switch (char1) {
+                case 'n' -> verticalDirection++;
+                case 's' -> verticalDirection--;
+                case 'e' -> horizontalDirection++;
+                case 'w' -> horizontalDirection--;
+            }
+        }
+        return (verticalDirection == 0 && horizontalDirection == 0);
+    }
+}
